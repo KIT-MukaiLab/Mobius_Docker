b3613d0b689979c64eaeeecbd875566ee18a46f6
kw21334w <b1748606@planet.kanazawa-it.ac.jp>
2022-11-08 21:49:13 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo docker-compose up --no-start
+
+echo -e "\n===========Result of Mobius docker install==============\n"
+sudo docker-compose ps
+echo -e "\n========================================================\n"
